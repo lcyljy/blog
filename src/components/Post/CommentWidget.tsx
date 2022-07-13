d6998d1d928e1fe3637b2885272122f8ea1a5aa9
lcyljy <35355087+lcyljy@users.noreply.github.com>
2022-07-13 23:57:38 +0900

    commentwidget 링크 수정

--- src/components/Post/CommentWidget.tsx
+++ src/components/Post/CommentWidget.tsx
@@ -2,7 +2,7 @@ import React, { createRef, FunctionComponent, useEffect } from 'react'
 import styled from '@emotion/styled'
 
 const src = 'https://utteranc.es/client.js'
-const repo = 'lcyljy/blog.github.io' // 자신 계정의 레포지토리로 설정
+const repo = 'https://lcyljy.github.io/gatsbyblog/' // 자신 계정의 레포지토리로 설정
 
 const UtterancesWrapper = styled.div`
   @media (max-width: 768px) {
